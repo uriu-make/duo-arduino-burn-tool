223eb7eeefab55d659522fa4c45f5838a056bd61
uriu-make <yoshinori.3775@outlook.jp>
2024-12-01 21:51:26 +0900

    Divide the process into functions

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <sys/select.h>
 #include <sys/stat.h>
+#include <stdint.h>
 
 #define CHUNK_SIZE 64
 
@@ -30,10 +31,19 @@
 #define UPDATE_CMD  ((unsigned char[]){0xAA, 0x55, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00})
 #define DATAEND_CMD ((unsigned char[]){0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF})
 
+bool rcv_none(int fd, uint8_t *buf, int len);
+bool rcv_connect(int fd, uint8_t *buf, int len);
+bool rcv_stop(int fd, uint8_t *buf, int len);
+bool rcv_start(uint8_t *buf, int len);
+bool rcv_update(uint8_t *buf, int len, uint32_t *file_size);
+bool rcv_dataend(uint8_t *buf, int len, FILE *file);
+
+bool remoteproc_init();
+bool remoteproc_start(void);
+bool remoteproc_stop(void);
+
 int main(int argc, char *argv[]) {
   int serial_fd = 0;
-  int state_fd = 0;
-  int fd = 0;
 
   struct termios tio;
   int baudRate = B9600;  // 9600bps
@@ -42,10 +52,13 @@ int main(int argc, char *argv[]) {
   fd_set rfds;
   int ret = 0;
 
-  int i;
-  unsigned char buf[CHUNK_SIZE];  // Serial receive buffer
-  uint32_t file_size = 0;         // Received file size
-  FILE *file;
+  uint8_t buf[CHUNK_SIZE];  // Serial receive buffer
+  uint32_t file_size = 0;   // Received file size
+  FILE *file = NULL;
+
+  if (false == remoteproc_init()) {
+    return -1;
+  }
 
   serial_fd = open(SERIAL_PORT, O_RDWR);
   if (serial_fd < 0) {
@@ -67,33 +80,6 @@ int main(int argc, char *argv[]) {
 
   ioctl(serial_fd, TCSETS, &tio);
 
-  state_fd = open(REMOTEPROC_STATE, O_RDWR);
-  if (state_fd < 0) {
-    return -1;
-  }
-  fd = open(FIRMWARE_CLASS, O_RDWR);
-  if (fd < 0) {
-    return -1;
-  }
-  write(fd, FIRMWARE_FILE_PATH, sizeof(FIRMWARE_FILE_PATH));
-  close(fd);
-
-  fd = open(REMOTEPROC_FIRMWARE, O_RDWR);
-  if (fd < 0) {
-    return -1;
-  }
-  write(fd, FIRMWARE_FILE_NAME, sizeof(FIRMWARE_FILE_NAME));
-  close(fd);
-
-  if (access(FIRMWARE_FILE_PATH "/" FIRMWARE_FILE_NAME, F_OK) == 0) {
-    state_fd = open(REMOTEPROC_STATE, O_RDWR);
-    if (state_fd < 0) {
-      return -1;
-    }
-    write(state_fd, START, sizeof(START));
-    close(state_fd);
-  }
-
   while (true) {
     FD_ZERO(&rfds);
     FD_SET(serial_fd, &rfds);
@@ -104,111 +90,44 @@ int main(int argc, char *argv[]) {
       exit(0);
     }
     int len = read(serial_fd, buf, sizeof(buf));
+
     if (0 < len) {
       // NONE
-      if (len == sizeof(NONE_CMD)) {
-        for (i = 0; i < len; i++) {
-          if (buf[i] != NONE_CMD[i]) {
-            break;
-          }
-        }
-        if (i == len) {
-          write(serial_fd, buf, len);
-          continue;
-        }
+      if (true == rcv_none(serial_fd, buf, len)) {
+        continue;
       }
 
       // Connection
-      if (len == sizeof(CONNECT_CMD)) {
-        for (i = 0; i < len; i++) {
-          if (buf[i] != CONNECT_CMD[i]) {
-            break;
-          }
-        }
-        if (i == len) {
-          write(serial_fd, buf, len);
-          continue;
-        }
+      if (true == rcv_connect(serial_fd, buf, len)) {
+        continue;
       }
 
       // STOP RrmoteProc
-      if (len == sizeof(STOP_CMD)) {
-        for (i = 0; i < len; i++) {
-          if (buf[i] != STOP_CMD[i]) {
-            break;
-          }
-        }
-        if (i == len) {
-          state_fd = open(REMOTEPROC_STATE, O_RDWR);
-          if (state_fd < 0) {
-            return -1;
-          }
-
-          write(state_fd, STOP, sizeof(STOP));
-          write(serial_fd, buf, len);
-
-          close(state_fd);
-          continue;
-        }
+      if (true == rcv_stop(serial_fd, buf, len)) {
+        continue;
       }
 
       // START RrmoteProc
-      if (len == sizeof(START_CMD)) {
-        for (i = 0; i < len; i++) {
-          if (buf[i] != START_CMD[i]) {
-            break;
-          }
-        }
-        if (i == len) {
-          if (access(FIRMWARE_FILE_PATH "/" FIRMWARE_FILE_NAME, F_OK) == 0) {
-            state_fd = open(REMOTEPROC_STATE, O_RDWR);
-            if (state_fd < 0) {
-              return -1;
-            }
-            write(state_fd, START, sizeof(START));
-            close(state_fd);
-          }
-
-          continue;
-        }
+      if (true == rcv_start(buf, len)) {
+        continue;
       }
 
       // UPDATE_COMMAND
-      if (len == sizeof(UPDATE_CMD)) {
-        for (i = 0; i < 4; i++) {
-          if (buf[i] != UPDATE_CMD[i]) {
-            break;
-          }
-        }
-        // printf("%d\n", file_size);
-        if (i == 4) {
-          file_size |= ((uint32_t)buf[4] << 0);
-          file_size |= ((uint32_t)buf[5] << 8);
-          file_size |= ((uint32_t)buf[6] << 16);
-          file_size |= ((uint32_t)buf[7] << 24);
-
-          file = fopen(FIRMWARE_FILE_PATH "/" FIRMWARE_FILE_NAME, "wb");
-          update = true;
-          continue;
-        }
+      if (true == rcv_update(buf, len, &file_size)) {
+        update = true;
+        file = fopen(FIRMWARE_FILE_PATH "/" FIRMWARE_FILE_NAME, "wb");
+        continue;
       }
 
       // End Command
-      if (len == sizeof(DATAEND_CMD)) {
-        for (i = 4; i < len; i++) {
-          if (buf[i] != DATAEND_CMD[i]) {
-            break;
-          }
-        }
-        if (i == len) {
-          fclose(file);
-          continue;
-        }
+      if (true == rcv_dataend(buf, len, file)) {
+        continue;
       }
 
       if (update == true) {
         fwrite(buf, len, 1, file);
         file_size -= len;
+
         if (file_size == 0) {
           update = false;
         }
@@ -219,4 +138,146 @@ int main(int argc, char *argv[]) {
 
   close(serial_fd);
   return 0;
+}
+
+bool rcv_none(int fd, uint8_t *buf, int len) {
+  int i = 0;
+  if (len == sizeof(NONE_CMD)) {
+    for (i = 0; i < len; i++) {
+      if (buf[i] != NONE_CMD[i]) {
+        return false;
+      }
+    }
+    write(fd, buf, len);
+    return true;
+  }
+  return false;
+}
+
+bool rcv_connect(int fd, uint8_t *buf, int len) {
+  int i = 0;
+  if (len == sizeof(CONNECT_CMD)) {
+    for (i = 0; i < len; i++) {
+      if (buf[i] != CONNECT_CMD[i]) {
+        return false;
+      }
+    }
+    write(fd, buf, len);
+    return true;
+  }
+  return false;
+}
+
+bool rcv_stop(int fd, uint8_t *buf, int len) {
+  int i = 0;
+  int state_fd = 0;
+  if (len == sizeof(STOP_CMD)) {
+    for (i = 0; i < len; i++) {
+      if (buf[i] != STOP_CMD[i]) {
+        return false;
+      }
+    }
+    remoteproc_stop();
+    write(fd, buf, len);
+    return true;
+  }
+  return false;
+}
+
+bool rcv_start(uint8_t *buf, int len) {
+  int i = 0;
+  int state_fd = 0;
+  if (len == sizeof(START_CMD)) {
+    for (i = 0; i < len; i++) {
+      if (buf[i] != START_CMD[i]) {
+        return false;
+      }
+    }
+    if (i == len) {
+      remoteproc_start();
+      return true;
+    }
+  }
+  return false;
+}
+
+bool rcv_update(uint8_t *buf, int len, uint32_t *file_size) {
+  int i = 0;
+
+  if (len == sizeof(UPDATE_CMD)) {
+    for (i = 0; i < 4; i++) {
+      if (buf[i] != UPDATE_CMD[i]) {
+        return false;
+      }
+    }
+    if (i == 4) {
+      *file_size |= ((uint32_t)buf[4] << 0);
+      *file_size |= ((uint32_t)buf[5] << 8);
+      *file_size |= ((uint32_t)buf[6] << 16);
+      *file_size |= ((uint32_t)buf[7] << 24);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool rcv_dataend(uint8_t *buf, int len, FILE *file) {
+  int i = 0;
+  if (len == sizeof(DATAEND_CMD)) {
+    for (i = 4; i < len; i++) {
+      if (buf[i] != DATAEND_CMD[i]) {
+        return false;
+      }
+    }
+    if (i == len) {
+      fclose(file);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool remoteproc_init() {
+  bool ret;
+  int class_fd = open(FIRMWARE_CLASS, O_RDWR);
+  if (class_fd < 0) {
+    return false;
+  }
+  write(class_fd, FIRMWARE_FILE_PATH, sizeof(FIRMWARE_FILE_PATH));
+  close(class_fd);
+
+  int firmware_fd = open(REMOTEPROC_FIRMWARE, O_RDWR);
+  if (firmware_fd < 0) {
+    return false;
+  }
+  write(firmware_fd, FIRMWARE_FILE_NAME, sizeof(FIRMWARE_FILE_NAME));
+  close(firmware_fd);
+
+  ret = remoteproc_start();
+  return ret;
+}
+
+bool remoteproc_start(void) {
+  if (access(FIRMWARE_FILE_PATH "/" FIRMWARE_FILE_NAME, F_OK) == 0) {
+    int fd = open(REMOTEPROC_STATE, O_RDWR);
+    if (fd < 0) {
+      return false;
+    }
+    write(fd, START, sizeof(START));
+    close(fd);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool remoteproc_stop(void) {
+  int fd = open(REMOTEPROC_STATE, O_RDWR);
+  if (fd < 0) {
+    return false;
+  }
+
+  write(fd, STOP, sizeof(STOP));
+  close(fd);
+  return true;
 }
